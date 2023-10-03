f8ac57915eae4a86e5ad637a221e74f8127d14c1
rasaenluis3 <rasaenluis3@unirioja.es>
2023-10-03 16:46:03 +0200

    Comit an pus

--- src/main/java/main/LanzameLosWebOS.java
+++ src/main/java/main/LanzameLosWebOS.java
@@ -1,22 +1,60 @@
 package main;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.URI;
+import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import javax.ws.rs.core.UriBuilder;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import com.sun.net.httpserver.HttpServer;
 
 public class LanzameLosWebOS {
-        public static void main(String[] args) {
-            URI baseUri = UriBuilder.fromUri("http://localhost/").port(8080).build();
-            ResourceConfig config = new ResourceConfig(RESTo_de_dividir.class);
-            HttpServer server = JdkHttpServerFactory.createHttpServer(baseUri, config);
+        private final static String htemele = "web\\pito.html";
+        public static void main(String[] args) throws IOException {
+            HttpServer server = HttpServer.create(new InetSocketAddress("localhost",8080),0);
+            ExecutorService ex = Executors.newCachedThreadPool();
+            server.setExecutor(ex);
+
+            server.createContext("/projektAEMEKU", new HTMLReturner(htemele));
+            server.createContext("/ejemplo", new HTMLReturner("web\\pito2.html"));
+            server.start();
+
             System.out.println("Pulsa enter para parar el servicio");
-            try {
-                new DataInputStream(System.in).readLine();
-            } catch (IOException e) { }
+            new Scanner(System.in).nextLine();
             System.out.println("Parando...");
             server.stop(0);
+            ex.shutdown();
         }
+}
+
+class HTMLReturner implements HttpHandler {
+    private final String htemelePath;
+    public HTMLReturner(String htemele) { htemelePath = htemele; }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        BufferedReader fInput = new BufferedReader(new FileReader(htemelePath));
+        StringBuilder bob = new StringBuilder();
+        String str;
+        while ((str = fInput.readLine())!=null) {
+            bob.append(str);
+        }
+        fInput.close();
+        exchange.sendResponseHeaders(200, bob.length());
+        BufferedWriter pito = new BufferedWriter( new OutputStreamWriter(exchange.getResponseBody()));
+        pito.write(bob.toString());
+        pito.flush();
+        pito.close();
+        System.out.println(exchange.getRequestMethod());
+        exchange.getResponseHeaders().forEach((a,b) -> {
+            System.out.println(a + " -- " + b);
+        });
+        exchange.close();
+    }
 }
